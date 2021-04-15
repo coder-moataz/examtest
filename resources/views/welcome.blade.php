01b0d8eca5a72bedeefb0dc7197313a7eea67ce8
unknown <coder.moataz.amr@gmail.com>
2021-04-15 14:57:15 +0200

    first commit

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>   
 <html>
 <head>
-	<link rel="stylesheet" type="text/css" href="{{ URL::asset('public/css/test.css') }}" />
+	<link rel="stylesheet" type="text/css" href="{{ secure_asset('css/test.css') }}" />
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
      <title>Quiz app</title> 
 </head>
